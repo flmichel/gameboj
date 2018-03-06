8a05912930ada63cd4c00af319ddc647f95d570e
Francois Michel <Francois.L.Michel@gmail.com>
2018-03-06 14:53:05 +0100

    ajout de tests

---
+++ src/ch/epfl/gameboj/RegisterFile.java
@@ -0,0 +1,23 @@
+package ch.epfl.gameboj;
+
+public final class RegisterFile<E extends register> {
+    RegisterFile(E[] allRegs) {
+        
+    }
+    
+    public int get(E reg) {
+        
+    }
+    
+    public void set(E reg, int newValue) {
+        
+    }
+    
+    boolean testBit(E reg, Bit b) {
+        
+    }
+    
+    void setBit(E reg, Bit bit, boolean newValue) {
+        
+    }
+}
