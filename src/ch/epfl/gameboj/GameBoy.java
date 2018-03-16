be2ba0fb149198cd2728d885dc74620673136081
Amshingafum <33152490+Amshingafum@users.noreply.github.com>
2018-03-16 15:24:00 +0100

    Update GameBoy.java
    GameBoy etape 5

--- src/ch/epfl/gameboj/GameBoy.java
+++ src/ch/epfl/gameboj/GameBoy.java
@@ -1,21 +1,46 @@
 package ch.epfl.gameboj;
 
+import ch.epfl.gameboj.component.cpu.Cpu;
 import ch.epfl.gameboj.component.memory.Ram;
 import ch.epfl.gameboj.component.memory.RamController;
 
 public class GameBoy {
-    
+
     private Bus bus;
     private Ram workRam;
+    private Cpu cpu;
+    private long cyclesNb = 0;
 
     public GameBoy(Object cartridge) {
         workRam = new Ram(AddressMap.WORK_RAM_SIZE);
+        cpu = new Cpu();
         bus = new Bus();
         bus.attach(new RamController(workRam, AddressMap.WORK_RAM_START));
         bus.attach(new RamController(workRam, AddressMap.ECHO_RAM_START, AddressMap.ECHO_RAM_END));
+        cpu.attachTo(bus);
     }
-    
+
     public Bus bus() {
         return bus;
     }
+
+    public Cpu cpu() {
+        return cpu;
+    }
+
+    public long cycles() {
+        return cyclesNb;
+    }
+
+    public void runUntil(long cycle) {
+       Preconditions.checkArgument(cycles() <= cycle);        
+       
+       if (cycles() < cycle) {
+            for (long i = cycles() ; i < cycle ; i++) {
+                cpu.cycle(i);
+                cyclesNb++;
+            }
+        }
+    }
+
 }
