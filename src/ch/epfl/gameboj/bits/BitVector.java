5f51d3a5f5d4c0538633ff511be5bea387fe8ea7
Amshingafum <andre.riandszaniecki@epfl.ch>
2018-04-09 14:32:05 +0200

    classe BitVector

--- src/ch/epfl/gameboj/bits/BitVector.java
+++ src/ch/epfl/gameboj/bits/BitVector.java
@@ -1,5 +1,25 @@
 package ch.epfl.gameboj.bits;
 
+import java.util.ArrayList;
+
+import ch.epfl.gameboj.Preconditions;
+
 public final class BitVector {
 
+    private final int length;
+   // private final ArrayList<Integer> vect;
+    
+    public BitVector (int taille, boolean v) {
+        Preconditions.checkArgument(taille >= 0 && (taille % Integer.SIZE == 0));
+        int val = 0;
+        if(v) val = 1;
+        for (int i = 0 ; i < taille/Integer.SIZE ; i++) {
+            
+//            vect.add(val);
+        }
+        
+        
+        length = taille;
+    }
+    
 }
