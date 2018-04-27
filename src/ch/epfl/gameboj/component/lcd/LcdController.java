5cfb3af71eacc6e098b3bec31a6a84818471591c
Francois Michel <Francois.L.Michel@gmail.com>
2018-04-27 12:16:54 +0200

    création de la classe lcdCotroller

---
+++ src/ch/epfl/gameboj/component/lcd/LcdController.java
@@ -0,0 +1,66 @@
+package ch.epfl.gameboj.component.lcd;
+
+import ch.epfl.gameboj.AddressMap;
+import ch.epfl.gameboj.Preconditions;
+import ch.epfl.gameboj.Register;
+import ch.epfl.gameboj.RegisterFile;
+import ch.epfl.gameboj.component.Clocked;
+import ch.epfl.gameboj.component.Component;
+import ch.epfl.gameboj.component.cpu.Cpu;
+import ch.epfl.gameboj.component.memory.Ram;
+
+public class LcdController implements Component, Clocked {
+    
+    public static final int LCD_WIDTH = 160;
+    public static final int LCD_HEIGHT = 144;
+    
+    public final Cpu cpu;
+    private Ram videoRam = new Ram(AddressMap.VIDEO_RAM_SIZE);
+
+    private enum Reg implements Register {
+        LCDC, STAT, SCY, SCX, LY, LYC, DMA, BGP, OBP0, OBP1, WY, WX;
+    }
+    
+    RegisterFile<Reg> registerFile = new RegisterFile<>(Reg.values());    
+
+    
+    public LcdController(Cpu cpu) {
+        this.cpu = cpu;
+    }
+
+    @Override
+    public void cycle(long cycle) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    /**
+     * @throws IllegalArgumentException si address ne peut pas s'écrire avec 16 bits.
+     */
+    @Override
+    public int read(int address) {
+        Preconditions.checkBits16(address);
+        if (address >= AddressMap.VIDEO_RAM_START && address < AddressMap.VIDEO_RAM_END) {
+            return videoRam.read(address - AddressMap.HIGH_RAM_START);
+        }
+        if (address >= AddressMap.REGS_LCDC_START && address < AddressMap.REGS_LCDC_END) {
+            return registerFile.get(Reg.values()[address - AddressMap.REGS_LCDC_START]);
+        }
+    }
+
+    /**
+     * @throws IllegalArgumentException si address ne peut pas s'écrire avec 16 bits ou data ne peut pas s'écrire avec 8 bits.
+     */
+    @Override
+    public void write(int address, int data) {
+        Preconditions.checkBits16(address);
+        Preconditions.checkBits8(data); 
+        if (address >= AddressMap.VIDEO_RAM_START && address < AddressMap.VIDEO_RAM_END) {
+            videoRam.write(address - AddressMap.HIGH_RAM_START, data);
+        }
+        if (address >= AddressMap.REGS_LCDC_START && address < AddressMap.REGS_LCDC_END) {
+            registerFile.set(Reg.values()[address - AddressMap.REGS_LCDC_START], data);
+        }
+    }
+
+}
